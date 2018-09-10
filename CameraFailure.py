71f8dc30c891d2ca7d37e13e66770db2ad947d3f
Park-Jongil <41685875+Park-Jongil@users.noreply.github.com>
2018-09-10 17:25:24 +0900

    Add files via upload

--- CameraFailure.py
+++ CameraFailure.py
@@ -2,17 +2,24 @@ import sqlite3
 import sys
 from sqlite3 import Error
 
+Count = 0
+Limit = 0
+if (len(sys.argv) > 1) : Count = int(sys.argv[1])
+if (len(sys.argv) > 2) : Limit = int(sys.argv[2])
+
 db_file = "NaizDB.db"
 conn = sqlite3.connect(db_file)
-
-Count = int(sys.argv[1])
 cur = conn.cursor()
 cur.execute("select CameraID,count(CameraID),CameraName,ip_addr from StatusChange as A,CameraList as B where A.CameraName==B.name AND substr(ip_addr,1,3)=='10.' group by CameraID order by count(CameraID) desc")
 rows = cur.fetchall()
-
-i = 0
-for row in rows :
-    i = i + 1
-    print( row )
-    if (i==Count) : break
+if (len(rows)!=0) : 
+    i = 0
+    for row in rows :
+        i = i + 1
+        Chk = row[1]
+        if (Limit > 0) : 
+            if (int(Chk) > Limit) : print( row )
+        else:
+            print( row )
+        if (i==Count) : break
 
